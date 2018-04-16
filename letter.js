c24e4fb9abd6a0c25aeb0b69f959468156d5ac14
Will <apricotsenator@gmail.com>
2018-04-15 23:01:38 -0400

    letter.js works, still undefined in logic

--- letter.js
+++ letter.js
@@ -1,18 +1,32 @@
 var Letter = function (char, guessed, display) {
 	this.char = char;
 	this.guessed = guessed;
-	this.display = function () {
-		this.show = "";
-		if (this.guessed === false) {
-			this.show = "_";
-		} else {
-			this.show = this.char;
-		}
-	};
-
 };
+
+Letter.prototype.display = function () {
+	if (this.guessed === false) {
+		var show = "-";
+		console.log(show);
+
+	} else {
+		var show = this.char;
+		console.log(show);
+	}
+}
+
+Letter.prototype.check = function(arg1){
+	if (arg1 === this.char){
+		this.guessed= true;
+		this.display();
+	}else {
+		this.guessed = false;
+		this.display();
+	}
+}
+
 var A = new Letter("B", false);
-A.display(console.log(A.show));
+A.check("C");
+
 
 
 
--- word.js
+++ word.js
@@ -4,21 +4,17 @@ function Word(target, char, found, getLet, findWord, checkLetter, wordRender) {
 	this.target = target;
 	this.char = [];
 	this.found = false;
-
-	this.getLet = function() {
-		for (var i=0; i < this.target.length; i++) {
-			this.char.push( new letter(this.target[i]));
+}
+	
+Word.prototype.wordRender = function() {
+		var string = '';
+		for (var i=0; i < this.char.length; i++){
+			string += this.char[i].letterRender();
 		}
+		return string;
 	};
 
-	this.findWord = function() {
-		this.found = this.char.every(function(currLett) {
-			return currLett.appear;
-		});
-		return this.found;
-	};
-
-	this.checkLetter = function(guessLet) {
+Word.prototype.checkLetter = function(guessLet) {
 		var toReturn = 0;
 
 		for (var i = 0; i < this.char.length; i++) {
@@ -30,14 +26,17 @@ function Word(target, char, found, getLet, findWord, checkLetter, wordRender) {
 		return toReturn;
 	};
 
-	this.wordRender = function() {
-		var string = '';
-		for (var i=0; i < this.char.length; i++){
-			string += this.char[i].letterRender();
-		}
-		return string;
+Word.prototype.findWord = function() {
+		this.found = this.char.every(function(currLett) {
+			return currLett.appear;
+		});
+		return this.found;
 	};
 
-}
+Word.prototype.getLet = function() {
+		for (var i=0; i < this.target.length; i++) {
+			this.char.push( new letter(this.target[i]));
+		}
+	};
 
 module.exports = Word;
